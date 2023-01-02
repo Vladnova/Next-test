9c991b220ea7347b5360349a5736f88e488f81a2
Vladnova <novalenkov05@gmail.com>
2023-01-02 13:37:52 +0200

    added API

---
+++ pages/api/data/socials.js
@@ -0,0 +1,22 @@
+export const socials = [
+  {
+    id: 1,
+    icon: 'youtube',
+    path: 'https://www.youtube.com/',
+  },
+  {
+    id: 2,
+    icon: 'instagram',
+    path: 'https://instagram.com/',
+  },
+  {
+    id: 3,
+    icon: 'linkedin',
+    path: 'https://www.linkedin.com/in/vladislav-novalenko/',
+  },
+  {
+    id: 5,
+    icon: 'twitter',
+    path: 'https://twitter.com/',
+  },
+];
